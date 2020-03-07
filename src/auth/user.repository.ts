68c55322e002a5142238dec9f98b4529240e3933
Eric Cabrel <tericcabrel@yahoo.com>
2020-03-07 14:51:55 +0100

    Handle username unicity

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -12,6 +13,14 @@ export class UserRepository extends Repository<User> {
     user.username = username;
     user.password = password;
 
-    await user.save();
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists')
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }

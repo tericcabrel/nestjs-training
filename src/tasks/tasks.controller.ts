0289c81e4a8363d39e9c05b99c479716deafbaee
Eric Cabrel <tericcabrel@yahoo.com>
2020-03-07 12:07:11 +0100

    Add CreateTaskDto for create a task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -14,8 +15,9 @@ export class TasksController {
   }
 
   @Post()
-  //createTask(@Body() body): Task {
-  createTask(@Body('title') title: string, @Body('description') description: string): Task {
-    return this.tasksService.createTask(title, description);
+  // createTask(@Body() body): Task {
+  // createTask(@Body('title') title: string, @Body('description') description: string): Task {
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 }

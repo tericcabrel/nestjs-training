d044b30926859c68614e42dd37e0e6baf0a948c7
Eric Cabrel <tericcabrel@yahoo.com>
2020-03-07 16:42:53 +0100

    Setup file configuration for different environment

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  const serverConfig = config.get('server');
+
+  console.log(serverConfig);
 
   const port = 3000;
 

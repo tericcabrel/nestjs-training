28093c7109ec069ee123cfc791fd2b1f2097f2a3
Eric Cabrel <tericcabrel@yahoo.com>
2020-03-07 18:40:36 +0100

    Test deleteTask methods in TaskService

--- src/tasks/task.service.spec.ts
+++ src/tasks/task.service.spec.ts
@@ -3,7 +3,6 @@ import { TasksService } from './tasks.service';
 import { TaskRepository } from './task.repository';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './task-status.enum';
-import mock = jest.mock;
 import { NotFoundException } from '@nestjs/common';
 
 const mockUser = { id: 12, username : 'Test user' };
@@ -11,6 +10,8 @@ const mockUser = { id: 12, username : 'Test user' };
 const mockTaskRepository = () => ({
   getTasks: jest.fn(),
   findOne: jest.fn(),
+  createTask: jest.fn(),
+  delete: jest.fn(),
 });
 
 describe('TasksService', () => {
@@ -64,6 +65,42 @@ describe('TasksService', () => {
       taskRepository.findOne.mockResolvedValue(null);
 
       expect(tasksService.getTaskById(1, mockUser)).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('createTask', () => {
+    it('should calls taskRepository,create() and returns a result', async () => {
+      taskRepository.createTask.mockResolvedValue('someTask');
+
+      expect(taskRepository.createTask).not.toHaveBeenCalled();
+
+      const createTaskDto = { title: 'Task title', description: 'Test desc' };
+
+      const result = await tasksService.createTask(createTaskDto, mockUser);
+
+      expect(taskRepository.createTask).toHaveBeenCalledWith(createTaskDto, mockUser);
+      expect(result).toEqual('someTask');
+    });
+  });
+
+  describe('deleteTask', () => {
+    it('should call taskRepository.deleteTask() to delete a task', async () => {
+      taskRepository.delete.mockResolvedValue({ affected: 1 });
+
+      expect(taskRepository.delete).not.toHaveBeenCalled();
+
+      await tasksService.deleteTask(1, mockUser);
+
+      expect(taskRepository.delete).toHaveBeenCalledWith({
+        id: 1,
+        userId: mockUser.id
+      })
+    });
+
+    it('should throws an error as task coukd not found', () => {
+      taskRepository.delete.mockResolvedValue({ affected: 0 });
+
+      expect(tasksService.deleteTask(1, mockUser)).rejects.toThrow(NotFoundException);
     });
   });
 });

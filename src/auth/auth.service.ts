2a0a8ac7a9fd39752813e05931ec0bbd9e0381ac
Eric Cabrel <tericcabrel@yahoo.com>
2020-03-07 14:21:10 +0100

    Create Auth module and create Entity and Repository

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) { }
+}

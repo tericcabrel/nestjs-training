5914a406c8547058172cbe85509c058f2e173f66
Eric Cabrel <tericcabrel@yahoo.com>
2020-03-07 11:57:57 +0100

    Create and endpoint to create a task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
@@ -11,5 +11,11 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  //createTask(@Body() body): Task {
+  createTask(@Body('title') title: string, @Body('description') description: string): Task {
+    return this.tasksService.createTask(title, description);
   }
 }

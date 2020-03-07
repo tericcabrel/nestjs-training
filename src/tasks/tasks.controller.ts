01c49cfb84acd8a5b5caed49689a21cad6fa3dd2
Eric Cabrel <tericcabrel@yahoo.com>
2020-03-07 15:54:07 +0100

    Create relation between task and user and update task creation logic

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -18,6 +18,8 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
+import { User } from '../auth/user.entity';
+import { GetUser } from '../auth/get-user-decorator';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
@@ -40,8 +42,11 @@ export class TasksController {
   // createTask(@Body() body): Task {
   // createTask(@Body('title') title: string, @Body('description') description: string): Task {
   @UsePipes(ValidationPipe)
-  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-    return this.tasksService.createTask(createTaskDto);
+  createTask(
+    @Body() createTaskDto: CreateTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.tasksService.createTask(createTaskDto, user);
   }
 
   @Delete('/:id')
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,6 +6,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { DeleteResult } from 'typeorm';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { User } from '../auth/user.entity';
 
 @Injectable()
 export class TasksService {
@@ -20,8 +21,8 @@ export class TasksService {
     return this.taskRepository.getTasks(filterDto);
   }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    return this.taskRepository.createTask(createTaskDto);
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto, user);
   }
 
   async getTaskById(id: number): Promise<Task> {
